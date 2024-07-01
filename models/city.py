d0010336fcd2e7a6e38d5e7d2293885090e5c692
kevxcyj <kevcyjunior@gmail.com>
2024-07-01 11:05:15 -0500

    Updated city file to add the relationship between class attribute places and Place

--- models/city.py
+++ models/city.py
@@ -8,7 +8,15 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete', backref='cities')
+    else:
+        state_id = ""
+        name = ""
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)

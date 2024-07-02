7660ffe868c3aa916d6760c00698ee26c01109da
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-02 17:34:27 +0000

    corrected dictionary and added models import stora

--- models/base_model.py
+++ models/base_model.py
@@ -53,10 +53,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
+        dictionary.pop('_sa_instance_state')
         return dictionary
+
 
     def delete(self):
         '''delete the current instance from the storage'''
-        models.storage.delete()
+        from models import storage
+        storage.delete(self)

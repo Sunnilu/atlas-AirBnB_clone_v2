5b41f3d0ee4c2b8ffe74edd11c06b688d3d70525
kevxcyj <kevcyjunior@gmail.com>
2024-07-02 12:41:17 -0500

    Added imports to user.py file

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """ Represents the users login using  """
 
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 class User(BaseModel):
     """ Class User """

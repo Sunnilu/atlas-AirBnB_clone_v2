cc26556260316998c0fb634e41e5281e530317b5
kevxcyj <kevcyjunior@gmail.com>
2024-07-03 01:02:56 -0500

    changed if statement and added import config

--- models/city.py
+++ models/city.py
@@ -4,12 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 import models
+from config import storage_t
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if models.storage_t == "db":
+    if storage_t == "db":
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)

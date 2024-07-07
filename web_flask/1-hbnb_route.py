73dfc7cab70f15f44504055a369da4a3f5fd7ebe
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-07 23:51:18 +0000

    removed true and debug

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def home():
     return "Hello HBNB!"
 
-app.route('/hbnb/', strict_slashes=True) 
+app.route('/hbnb/', strict_slashes=False) 
 def hbnb():
     return "HBNB"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

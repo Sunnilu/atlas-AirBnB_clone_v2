fbabf1e1cade4791f16eba2c6577558931cab093
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-07 23:57:19 +0000

    added url

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,12 +6,13 @@ start a flask web application
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes=False
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def home():
     return "Hello HBNB!"
 
-app.route('/hbnb/', strict_slashes=False) 
+app.route('/hbnb') 
 def hbnb():
     return "HBNB"
 

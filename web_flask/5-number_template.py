e606783af47f691267c07c622d167adad94ff7d1
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 16:37:55 +0000

    added line spaces

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 '''
-Script that starts a Flask web application:
+Script that starts a Flask web application: add 5th view func that displays HTML
 '''
+
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def c(text):
     # Replace underscores with spaces
     text = text.replace('_', ' ')
     return "C {}".format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, methods=['GET'], strict_slashes=False)
 @app.route('/python/<text>', methods=['GET'], strict_slashes=False)
@@ -28,6 +34,7 @@ def python(text):
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
+
 @app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
 def number(n):
     if isinstance(n, int):
@@ -35,12 +42,14 @@ def number(n):
     else:
         return "Invalid input. '{}' is not an integer.".format(n)
 
+
 @app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
         return render_template('number.html', number=n)
     else:
         return "Invalid input. '{}' is not an integer.".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

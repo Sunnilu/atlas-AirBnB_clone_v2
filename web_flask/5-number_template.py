f402c3206d63ed62112fe89128f9d7a79e0fdc96
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 17:18:53 +0000

    added changes to 5 with comments

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,54 +1,53 @@
 #!/usr/bin/python3
-'''
-Script that starts a Flask web application: add 5th view func that displays HTML
-'''
+""" 5. Add fifth view func that displays HTML page if n is int """
 
-
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def home():
-    return "Hello HBNB!"
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', methods=['GET'], strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
-def c(text):
-    # Replace underscores with spaces
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
     text = text.replace('_', ' ')
-    return "C {}".format(text)
+    return 'C {}'.format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, methods=['GET'], strict_slashes=False)
-@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
-def python(text):
-    # Replace underscores with spaces
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
     text = text.replace('_', ' ')
-    return "Python {}".format(text)
+    return 'Python {}'.format(text)
 
 
-@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
-def number(n):
-    if isinstance(n, int):
-        return "{} is a number".format(n)
-    else:
-        return "Invalid input. '{}' is not an integer.".format(n)
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
+    return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
-def number_template(n):
-    if isinstance(n, int):
-        return render_template('number.html', number=n)
-    else:
-        return "Invalid input. '{}' is not an integer.".format(n)
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

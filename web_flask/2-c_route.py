0af111e6324d1ad96b504096359e6509880c8490
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:12:28 +0000

    clean up of line spaces

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-'''
-Script that starts a Flask web application:
-'''
+""" Flask web application with 3 view functions added """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -15,17 +14,15 @@ def home():
 
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
-
     return "HBNB"
 
 
 @app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def c(text):
     # Replace underscores with spaces
-
-
     text = text.replace('_', ' ')
     return "C {}".format(text)
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

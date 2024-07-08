9676181a2e5fa84bcacb8aa26d5ab54527c24b62
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:59:44 +0000

    updated cities in the state.py for DBstorage

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """This is the state class"""
 import os
-import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models.city import City
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-        cities = relationship between state and city tables.
-    """
-
     __tablename__ = 'states'
+
+    # Attributes for both types of storage
+    name = Column(String(128), nullable=False)
+
+    # Define relationship differently based on storage type
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', back_populates='state',
-            cascade='all, delete, delete-orphan')
-
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
     else:
-        name = ""
-
         @property
         def cities(self):
-            """returns list of Cities and some relationships"""
-            cities_instances = []
-            cities_dict = models.storage.all(models.City)
-            for key, value in cities_dict.items():
-                if self.id == value.state_id:
-                    cities_instances.append(value)
-            return cities_instances
+            """
+            Getter method for cities linked to the current State
+            """
+            from models import storage  # Ensure this import is correctly set for your project
+            city_objs = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_objs.append(city)
+            return city_objs
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        """Return string representation of State"""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)

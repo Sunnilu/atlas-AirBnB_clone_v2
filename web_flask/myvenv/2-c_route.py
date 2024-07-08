a4b1b55ebc222c273e02cc4629b1c6d1e8c507d3
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 03:55:59 +0000

    2c-route

---
+++ web_flask/myvenv/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+script that starts a Flask web application:
+'''
+
+from flask import Flask
+
+app = Flask(__name__, strict_slashes=False)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', methods=['GET'])
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', methods=['GET'])
+def c(text):
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

123543b4256b6be4292bb686f68dede5cb601dc2
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:08:32 +0000

    removed white spaces

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,7 +15,7 @@ def home():
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
     return "HBNB"
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

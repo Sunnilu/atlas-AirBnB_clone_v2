37a14c41f74ef4f0569eb75c867f435cbcf637fc
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:03:23 +0000

    line spaces corrected

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-'''
-start a flask web application
-'''
+""" start of Flask web application"""
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
 

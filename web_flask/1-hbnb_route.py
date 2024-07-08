510577db6375644fea1e22089e07c3d376720256
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:07:21 +0000

    line spaces added

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-'''
-Script that starts a Flask web application:
-'''
+""" Flask web application with 2 commands """
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
     return "HBNB"
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

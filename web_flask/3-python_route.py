741c14f047c1beb32d331bab74ccc0bf456e71ed
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-08 21:28:21 +0000

    added line spaces

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-'''
-Script that starts a Flask web application:
-'''
+""" Flask web application func that redirects and has default al for variable """
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def c(text):
@@ -21,12 +23,14 @@ def c(text):
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, methods=['GET'], strict_slashes=False)
 @app.route('/python/<text>', methods=['GET'], strict_slashes=False)
 def python(text):
     # Replace underscores with spaces
     text = text.replace('_', ' ')
     return "Python {}".format(text)
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

683623b3bf035cb7c65a64c8496b9106c8817e97
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-10 00:53:05 +0000

    state.py rewrite

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,35 +1,12 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import os
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import relationship
 
+from models.city import City  
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-        cities = relationship between state and city tables.
-    """
+class State(BaseModel):  #  State inherits from BaseModel
+    # Other attributes and methods...
 
-    __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', back_populates='state',
-            cascade='all, delete, delete-orphan')
-
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """returns list of Cities and some relationships"""
-            cities_instances = []
-            cities_dict = models.storage.all(models.City)
-            for key, value in cities_dict.items():
-                if self.id == value.state_id:
-                    cities_instances.append(value)
-            return cities_instances
+    @property
+    def cities(self):
+        # a relationship set up between State and City
+        # This method returns a list of City objects associated with the current State instance
+        return [city for city in self.__session.query(City).filter(City.state_id == self.id)]

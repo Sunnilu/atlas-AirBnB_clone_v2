ff339e20e8d035b8f2b053207525fb6960de9ae8
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-10 00:47:18 +0000

    changes on line spaces

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,12 +3,15 @@
 Starts a Flask web application
 """
 
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 
+
 app = Flask(__name__)
+
 
 
 @app.route('/states', strict_slashes=False)
@@ -16,15 +19,16 @@ def states_list():
     """Display a HTML page listing all State objects"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-
     return render_template('9-states.html', states=sorted_states)
+
 
 
 @app.route('/states/<state_id>', strict_slashes=False)
 def state_cities(state_id):
     """Display a HTML page listing cities of a specific State"""
     state = storage.get(State, state_id)
-    
+
+
     if state is None:
         return render_template('9-states.html', state_id="Not found!")
 
